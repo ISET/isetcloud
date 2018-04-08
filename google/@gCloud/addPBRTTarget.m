cc5ee94decdeacbbadfceca28577eabecb559bb3
Henryk Krzysztof Blasinski <hblasins@stanford.edu>
2018-04-07 20:24:08 -0700

    Moved files to make the directory structure more consistent. Fixed minor bugs in cloud rendering. Removed the 'cleanup' job from k8s setup. This job is no longer supported due to permission changes.

---
+++ google/@gCloud/addPBRTTarget.m
@@ -0,0 +1,31 @@
+function target = addPBRTTarget(obj,thisR)
+% Add a PBRT target job to the google cloud engine instance
+%
+% ZL Vistateam, 2017
+
+%% Parse
+
+
+%% Act
+
+% When we loop and create various versions of the PBRT scene file, we
+% put all the versions into the same remote directory.  So we set up
+% the cloud folder on the input.
+pbrtScene = thisR.get('input file');
+if ~exist(pbrtScene,'file'), error('PBRT scene not found %s\n',pbrtScene);
+else,                       [~, sceneName] = fileparts(pbrtScene);
+end
+cloudFolder = fullfile(obj.cloudBucket,obj.namespace,sceneName);
+
+% The output pbrt scene file is based on the output file, and this can
+% change.
+pbrtScene = thisR.get('output file');
+[~, sceneName] = fileparts(pbrtScene);
+target.camera = thisR.camera;
+target.local = pbrtScene;
+target.remote = fullfile(cloudFolder,sprintf('%s.pbrt',sceneName));
+
+% Add this target to the targets already stored.
+obj.targets = cat(1,obj.targets,target);
+
+end
