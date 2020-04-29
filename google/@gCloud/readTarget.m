0fa993df046267799c4dea28703bff0bda91293e
Zhenyi-Liu <zhenyiliu27@gmail.com>
2020-04-29 10:17:00 +0800

    read target.json and added the information to the gCloud object

---
+++ google/@gCloud/readTarget.m
@@ -0,0 +1,24 @@
+function obj = readTarget(obj,targetName)
+% Copy the flywheel information from target.json file to gcp.fwAPI.
+% Syntax
+%   gcp = gcp.readTarget(targetName);
+%
+% Description
+%   Parse a target.json file, copy the slots to a gcp object, so we can 
+%   re-render a scene without writing out and uploading the file to
+%   flywheel again.
+%
+% Input
+%   targetName: Path to the target.json for the scene.
+%
+% Zhenyi,2020
+%
+%%
+scene_target = jsonread(targetName);
+% I think there are all we need.
+obj.targets.fwAPI = scene_target.fwAPI;
+obj.targets.remote = scene_target.remote;
+obj.targets.local = scene_target.local;
+
+
+end
