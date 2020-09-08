7070bb9a7cd44bed5c42bbb5745ad5cf1b9c5182
David Cardinal <github@proshooters.com>
2020-09-08 13:19:04 -0700

    Add support for Windows by using the where command when ispc() instead of which

--- google/mcGcloudExists.m
+++ google/mcGcloudExists.m
@@ -1,9 +1,17 @@
 function [gcloudExists, status, result] = mcGcloudExists
 %% check whether we have google cloud sdk in our path
 
-[status_gcloud, result.gcloud] = system('which gcloud');
-[status_gsutil, result.gsutil] = system('which gsutil');
-[status, result.kubectl] = system('which kubectl');
+% Windows doesn't have a which command.
+% Many versions have a where command, though, so we can try to use it.
+if ~ispc
+    [status_gcloud, result.gcloud] = system('which gcloud');
+    [status_gsutil, result.gsutil] = system('which gsutil');
+    [status, result.kubectl] = system('which kubectl');
+else
+    [status_gcloud, result.gcloud] = system('where gcloud');
+    [status_gsutil, result.gsutil] = system('where gsutil');
+    [status, result.kubectl] = system('where kubectl');
+end
 
 gcloudExists = (0 == status==status_gcloud==status_gsutil);
 
